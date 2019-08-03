993c60841e4d3ee1a2a0ddcf953c9091a9e190db
unknown <omanovic.edo@gmail.com>
2019-08-03 11:53:58 +0200

    Added logging system  - spdlog

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,6 +7,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
 	
+	Hazel::Log::Init(); //move to engine init later
+	int a = 10;
+	HZ_CORE_WARN("initialize core log");
+	HZ_INFO("hello var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
